5bd5eb553ddcac673cf25ca8d99ae767ca1cf142
Long-Lost-s <hoanglong2117@gmail.com>
2025-02-04 17:36:13 +0700

    completed configuration and properties and a part of model

--- src/main/java/org/example/springsecurityhandmade/configuration/SecurityConfiguration.java
+++ src/main/java/org/example/springsecurityhandmade/configuration/SecurityConfiguration.java
@@ -1,4 +1,61 @@
 package org.example.springsecurityhandmade.configuration;
 
+import org.example.springsecurityhandmade.controller.CustomSuccessHandle;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return authenticationProvider::authenticate;
+    }
+
+    @Bean
+    public CustomSuccessHandle customSuccessHandle() {
+        return new CustomSuccessHandle();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(
+                        auth -> auth
+                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                .requestMatchers("/user/**").hasRole("USER")
+                                .anyRequest().authenticated()
+                )
+                .formLogin(f -> f.successHandler(customSuccessHandle()))
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
 }
---
+++ src/main/java/org/example/springsecurityhandmade/controller/CustomSuccessHandle.java
@@ -0,0 +1,58 @@
+package org.example.springsecurityhandmade.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import java.io.IOException;
+import java.util.List;
+
+public class CustomSuccessHandle extends SimpleUrlAuthenticationSuccessHandler {
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+    @Override
+    protected void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        String targetUrl = determineTargetUrl(request, response, authentication);
+        if (response.isCommitted()) {
+            return;
+        }
+        redirectStrategy.sendRedirect(request, response, targetUrl);
+    }
+
+    protected String determineTargetUrl(Authentication authentication) {
+        String url;
+        if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_DBA"))) {
+            url = "/dba";
+        } else if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN"))) {
+            url = "/admin";
+        } else if (authentication.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_USER"))) {
+            url = "/user";
+        } else {
+            url = "/accessDenied";
+        }
+        return url;
+    }
+
+    private boolean isDba(List<String> roles) {
+        return roles.contains("DBA");
+    }
+
+    private boolean isAdmin(List<String> roles) {
+        return roles.contains("ADMIN");
+    }
+
+    private boolean isUser(List<String> roles) {
+        return roles.contains("USER");
+    }
+
+    public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
+        this.redirectStrategy = redirectStrategy;
+    }
+
+    public RedirectStrategy getRedirectStrategy() {
+        return redirectStrategy;
+    }
+}
